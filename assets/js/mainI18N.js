6e30f6b1a0428c16c1d235e176311562ec7a22dd
constantin <33323432+costa320@users.noreply.github.com>
2021-03-09 12:26:56 +0100

    _cookie feature deleted

--- assets/js/mainI18N.js
+++ assets/js/mainI18N.js
@@ -3,60 +3,60 @@ var initialConfig;
 
 /* INITIALIZATION */
 try {
-  /* if it cant retriev enough configuration then will retry with "inizializationDefault" */
-  initialConfig = CookieLngDetection(initOptI18N);
-  i18next.use(i18nextHttpBackend).init(initialConfig);
+    /* if it cant retriev enough configuration then will retry with "inizializationDefault" */
+    initialConfig = CookieLngDetection(initOptI18N);
+    i18next.use(i18nextHttpBackend).init(initialConfig);
 } catch (err) {
-  initialConfig = CookieLngDetection(inizializationDefault);
-  i18next.use(i18nextHttpBackend).init(initialConfig);
+    initialConfig = CookieLngDetection(inizializationDefault);
+    i18next.use(i18nextHttpBackend).init(initialConfig);
 }
 /* END INITIALIZATION */
 
 Logger(["CONFIGURATION IN USE:", initialConfig]);
 
 function updateContent() {
-  if (initialConfig.testScripts) _PerformanceTests("start");
-  stopObserving();
-  try {
-    /* gets all elements that needs some kind of translation */
-    let elList = document.body.querySelectorAll("[data-lang]");
-    /* just for IE */
-    elList = Array.from(elList);
+    if (initialConfig.testScripts) _PerformanceTests("start");
+    stopObserving();
+    try {
+        /* gets all elements that needs some kind of translation */
+        let elList = document.body.querySelectorAll("[data-lang]");
+        /* just for IE */
+        elList = Array.from(elList);
 
-    elList.forEach(function (elem, i) {
-      manageElementDataAttribute(elem, "lang");
-    });
+        elList.forEach(function (elem, i) {
+            manageElementDataAttribute(elem, "lang");
+        });
 
-    Logger([
-      "detected user language: ",
-      i18next.language,
-      " --> loaded languages: ",
-      initialConfig.whiteList.join(", "),
-    ]);
+        Logger([
+            "detected user language: ",
+            i18next.language,
+            " --> loaded languages: ",
+            initialConfig.whiteList.join(", "),
+        ]);
 
-    startObserving();
-  } catch (err) {
-    throw new Error(err);
-  }
-  if (initialConfig.testScripts) _PerformanceTests("stop");
+        startObserving();
+    } catch (err) {
+        throw new Error(err);
+    }
+    if (initialConfig.testScripts) _PerformanceTests("stop");
 }
 
 function changeLng(lng) {
-  i18next.changeLanguage(lng);
+    i18next.changeLanguage(lng);
 }
 
 /* I18N EVENTS */
 i18next.on("initialized", function (options) {
-  // init set content
-  updateContent();
+    // init set content
+    updateContent();
 });
 
 i18next.on("languageChanged", function (lang) {
-  updateContent();
+    updateContent();
 });
 
 i18next.on("loaded", function (loaded) {
-  Logger(["new resource loaded:", loaded]);
+    Logger(["new resource loaded:", loaded]);
 });
 
 /* END I18N EVENTS  */
@@ -69,88 +69,71 @@ i18next.on("loaded", function (loaded) {
 var mutationObserver;
 var observing = false;
 function startObserving() {
-  Logger(["STARTED Observing..."]);
-  mutationObserver = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      Logger([mutation]);
-      updateContent();
+    Logger(["STARTED Observing..."]);
+    mutationObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            Logger([mutation]);
+            updateContent();
+        });
     });
-  });
 
-  // Starts listening for changes in the root HTML element of the page.
-  mutationObserver.observe(document.documentElement, {
-    characterDataOldValue: false,
-    characterData: false,
-    childList: true,
-    subtree: true,
-    attributeOldValue: true,
-    attributes: true,
-    attributeFilter: ["data-lang"],
-  });
-  observing = true;
+    // Starts listening for changes in the root HTML element of the page.
+    mutationObserver.observe(document.documentElement, {
+        characterDataOldValue: false,
+        characterData: false,
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+        attributes: true,
+        attributeFilter: ["data-lang"],
+    });
+    observing = true;
 }
 
 function stopObserving() {
-  Logger(["STOPED Observing..."]);
-  if (observing) mutationObserver.disconnect();
-  observing = false;
+    Logger(["STOPED Observing..."]);
+    if (observing) mutationObserver.disconnect();
+    observing = false;
 }
 
 function Logger(messages) {
-  /* if debug is set to be true then logg  */
-  if (initialConfig && initialConfig.debug) {
-    console.log(messages);
-  }
+    /* if debug is set to be true then logg  */
+    if (initialConfig && initialConfig.debug) {
+        console.log(messages);
+    }
 }
 
 /* MIDDLEWARES */
 function CookieLngDetection(configuration) {
-  let lng = getCookie("sLang");
-  /*  1) search for language in passed configuration 
-      2) search for language in cookies
-      3) search for language using browser language detector of i18next
-  */
-  if (configuration && configuration.lng) {
-    return configuration;
-  } else if (lng) {
-    let newConfiguration = configuration;
-    newConfiguration.lng = lng;
-    return newConfiguration;
-  } else {
-    return configuration;
-  }
+    let lng = getCookie("sLang");
+    /*  1) search for language in passed configuration 
+        2) search for language in cookies
+        3) search for language using browser language detector of i18next
+    */
+    if (configuration && configuration.lng) {
+        return configuration;
+    } else if (lng) {
+        let newConfiguration = configuration;
+        newConfiguration.lng = lng;
+        return newConfiguration;
+    } else {
+        return configuration;
+    }
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    var name = cname + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
 
-function setCookie(cName, cValue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = "expires=" + d.toUTCString();
-  document.cookie = cName + "=" + cValue + ";" + expires + ";path=/";
-}
 
-function checkCookie() {
-  var user = getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-    user = prompt("Please enter your name:", "");
-    if (user != "" && user != null) {
-      setCookie("username", user, 365);
-    }
-  }
-}
